2a7a7c2f99cea2c3babd2e62b87b6a2355b2df9c
Vicente Ruiz <vicenteruiz@Vicentes-MBP.fritz.box>
2023-07-26 18:37:59 +0200

    find heaviest coded and tested

--- family_dictionary.py
+++ family_dictionary.py
@@ -1,10 +1,13 @@
-familia_ruiz = {'Vicente':[1.80,21474836471,52],'Fabiana':[1.70,65,49],'Victoria':[1.72,60,17],'Lucas':[1.80,75,15]}
+familia_ruiz = {'Vicente':[1.80,85,52],'Fabiana':[1.70,65,49],'Victoria':[1.72,60,17],'Lucas':[1.80,75,15]}
 
 oldest = 0
 oldest_name = "" 
 
 lightest = 1000
 lightest_name = ""
+
+heaviest = 0
+heaviest_name = ""
 
 for k,v in familia_ruiz.items():
     if v[2]>oldest:
@@ -20,3 +23,10 @@ for k,v in familia_ruiz.items():
         lightest_name = k
 
 print(lightest_name,lightest)
+
+for k,v in familia_ruiz.items():
+    if v[1]> heaviest:
+        heaviest = v[1]
+        heaviest_name = k
+
+print(heaviest_name,heaviest)
